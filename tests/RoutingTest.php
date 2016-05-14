55fc7f7466509d7c1756929b35b7abcf510934a3
apichart <aslovechart@gmail.com>
2016-05-14 11:08:53 +0700

    ทดสอบ
    ทดสอบ

--- tests/RoutingTest.php
+++ tests/RoutingTest.php
@@ -1,21 +1,7 @@
 <?php
 Class RoutingTest extends PHPUnit_Framework_TestCase {
 	public function testRouteApiLine(){
-
-		
-
-        // We need a request and response object to invoke the action
-		$environment = \Slim\Http\Environment::mock([
-			'REQUEST_METHOD' => 'GET',
-			'REQUEST_URI' => '/chat',
-			'QUERY_STRING'=>'lineID=apichartkhiaraiv&message=']
-			);
-		$request = \Slim\Http\Request::createFromEnvironment($environment);
-		$response = new \Slim\Http\Response();
-
-        // run the controller action and test it
-		
-		var_dump($response->withJson($request->getQueryParams())->getBody());
+		echo "test";
 	}
 }
 
